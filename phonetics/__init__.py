1e927a0eb965e5782717a695001201c5f3292617
Matthew Rutter-Moore <matt.moore@nchc.nhs.uk>
2023-12-13 12:01:30 +0000

    Updating Nameshout API function to accept multiple tokens
    To get around issue of tokens running out of free executions, the code has been adapted to cycle through a set of tokens to test each individually

--- phonetics/__init__.py
+++ phonetics/__init__.py
@@ -5,14 +5,20 @@ import json
 import requests
 import os
 
-NAMESHOUT_API_TOKEN = os.environ.get('NAMESHOUT_API_TOKEN')
-
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
+    # Retrieve the name from the GET parameter
     name = req.params.get('name')
     logging.info("Name: %s" % name)
 
+    # Define a list of our API tokens using settings variables
+    nameshout_api_tokens = [
+        os.environ.get('NAMESHOUT_API_TOKEN_1'),
+        os.environ.get('NAMESHOUT_API_TOKEN_2')
+    ]
+
+    # If no name was detected check the request body
     if not name:
         try:
             req_body = req.get_json()
@@ -21,32 +27,49 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         else:
             name = req_body.get('name')
 
+    # If the name has now been found
     if name:
-
+        # Check if a string
         if not isinstance(name,str):
             return func.HttpResponse(status_code=400)
-        
+        # Check if it's a reasonable length
         if not 0 < len(name) <= 255:
             return func.HttpResponse(status_code=400)
 
+        # Attempt to create a phonetical translation
         try:
+            # Set base string which shall be returned if translation does not work
             pronunciation = ""
-            bearer_token = NAMESHOUT_API_TOKEN
-            url = "https://api.nameshouts.com/api/v2.0/name/%s/english" % name
-            headers = {
-                'Authorization': "Bearer %s" % bearer_token,
-                'Content-Type': "application/x-www-form-urlencoded"
-            }
 
-            nameshout_response = requests.get(url, headers=headers)
+            # Cycle through each of the nameshout api tokens - some may have no more free executions
+            for bearer_token in nameshout_api_tokens:
 
-            if nameshout_response.status_code == 200:
+                # Create the HTTP request
+                url = "https://api.nameshouts.com/api/v2.0/name/%s/english" % name
+                headers = {
+                    'Authorization': "Bearer %s" % bearer_token,
+                    'Content-Type': "application/x-www-form-urlencoded"
+                }
 
-                json_response = nameshout_response.json()
-                name_res = json_response['name_res']
-   
-                if len(name_res) > 0:
-                    pronunciation = name_res[0]['name_phonetic']
+                nameshout_response = requests.get(url, headers=headers)
+                
+                # Determine response
+                if nameshout_response.status_code == 200:
+                    json_response = nameshout_response.json()
+
+                    # Check if request limit has been reached
+                    if 'search_limit_reached' in json_response:
+                        if json_response['search_limit_reached'] is True:
+                            pass
+
+                    else:
+                        # If not reached limit, then retrieve name then break for loop so that current phonetical spelling is returned
+                        name_res = json_response['name_res']
+        
+                        if len(name_res) > 0:
+                            pronunciation = name_res[0]['name_phonetic']
+
+                        break
 
         except ValueError:
             func.HttpResponse(status_code=500)
