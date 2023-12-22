2f48fa597c9ed3656c92f5d45c9bcfbe4cb622b5
Matthew Rutter-Moore <matt.moore@nchc.nhs.uk>
2023-12-22 11:00:04 +0000

    Adding previous pronunciation creation method as a back-up if nameshout free capacity is reached

--- phonetics/__init__.py
+++ phonetics/__init__.py
@@ -4,6 +4,55 @@ import logging
 import json
 import requests
 import os
+import pronouncing
+
+MAPPING = {
+    'AA0':'aa',
+    'AA1':'aa',
+    'AA2':'aa',
+    'AE0':'a',
+    'AE1':'a',
+    'AE2':'a',
+    'AH0':'ah',
+    'AH1':'ah',
+    'AH2':'ah',
+    'AO0':'ao',
+    'AO1':'ao',
+    'AO2':'ao',
+    'AW0':'aw',
+    'AW1':'aw',
+    'AW2':'aw',
+    'AY0':'ay',
+    'AY1':'ay',
+    'AY2':'ay',
+    'EH0':'eh',
+    'EH1':'eh',
+    'EH2':'eh',
+    'ER0':'er',
+    'ER1':'er',
+    'ER2':'er',
+    'EY0':'ey',
+    'EY1':'ey',
+    'EY2':'ey',
+    'IH0':'ih',
+    'IH1':'ih',
+    'IH2':'ih',
+    'IY0':'iy',
+    'IY1':'iy',
+    'IY2':'iy',
+    'OW0':'ow',
+    'OW1':'ow',
+    'OW2':'ow',
+    'OY0':'oy',
+    'OY1':'oy',
+    'OY2':'oy',
+    'UH0':'uh',
+    'UH1':'uh',
+    'UH2':'uh',
+    'UW0':'uw',
+    'UW1':'uw',
+    'UW2':'uw'
+}
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
@@ -82,6 +131,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
             if nameshout_success is False:
                 logging.info("Nameshout has no capacity")
+                pronunciation = custom_pronunciation(name)
 
         except ValueError:
             func.HttpResponse(status_code=500)
@@ -90,3 +140,24 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(json.dumps(pronunciation),status_code=200)
     else:
         return func.HttpResponse(status_code=400)
+    
+
+def custom_pronunciation(name) -> str:
+
+    pronunciation_characters = []
+    phonetics = pronouncing.phones_for_word(name)
+    logging.info("Phonetic Spelling: %s" % phonetics)
+
+    for phonetic in phonetics:
+        phonetic_characters = phonetic.split()
+        for character in phonetic_characters:
+            if character in MAPPING:
+                pronunciation_characters.append(MAPPING[character])
+            else:
+                pronunciation_characters.append(character)
+
+        break
+    
+    delimiter = '-'
+    pronunciation = delimiter.join(pronunciation_characters)
+    return pronunciation.lower()
